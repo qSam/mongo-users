81ca08bfd9d948d65e4731e7b0a9e7e343925694
Qasim Laghari <go.jets.laghari@gmail.com>
2017-04-18 22:11:08 -0500

    Create new user model and add Mocha test

--- src/user.js
+++ src/user.js
@@ -1,2 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+//Define Schema
+const UserSchema = new Schema({
+  name: String
+});
+//Create the model
+const User = mongoose.model('user', UserSchema);
+//Export the model
+module.exports = User;
---
+++ test/create_test.js
@@ -0,0 +1,5 @@
+describe('Creating records', () => {
+  it('saves a user', () => {
+    
+  });
+});

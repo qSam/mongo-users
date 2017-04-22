6d414313bfd60aec449e2f3215f31926560bf70d
Qasim Laghari <go.jets.laghari@gmail.com>
2017-04-21 21:39:00 -0500

    Add assertion to save a user

--- test/create_test.js
+++ test/create_test.js
@@ -4,6 +4,11 @@ const User = require('../src/user');
 describe('Creating records', () => {
   it('saves a user', () => {
     const joe = new User({name : 'Joe'});
-    joe.save();
+    joe.save()
+      .then(() => {
+        //Has Joe been saved successfully
+        assert(!joe.isNew);
+        done();
+      });
   });
 });
